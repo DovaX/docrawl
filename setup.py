e45fbcdc0ddc573bbce59905cb1c755fdee2ead5
Dominik Vach <vach.dominik@gmail.com>
2022-03-15 19:45:57 +0100

    update setup

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
     
 setuptools.setup(
     name='docrawl',
-    version='0.1.7',
+    version='0.1.9',
     author='DovaX',
     author_email='dovax.ai@gmail.com',
     description='Do automated crawling of pages using scrapy',
