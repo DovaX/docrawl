988653dd7caa1053374c25045930914be4ce793b
Dominik Vach <vach.dominik@gmail.com>
2024-03-14 10:33:35 +0100

    release 1.2.4

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
     
 setuptools.setup(
     name='docrawl',
-    version='1.2.3',
+    version='1.2.4',
     author='DovaX',
     author_email='dovax.ai@gmail.com',
     description='Do automated crawling of pages using scrapy',
